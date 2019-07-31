9e26199a6a39c5ca5b1373f8c1ef61560752b832
root <root@DESKTOP-1ML003O.localdomain>
2019-07-31 12:32:06 +1000

    This branch was used to add countries.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,35 @@
 module ApplicationHelper
     def city_select()
         options = [
-            ["Melbourne, Australia"],
-            ["Sydney, Australia"], 
-            ["Bristol, UK"],
-            ["London, UK"]
+            ["Melbourne"],
+            ["Sydney"], 
+            ["Brisbane"],
+            ["Hobart"],
+            ["Perth"],
+            ["Adelaide"],
+            ["Darwin"],
+            ["Auckland"],
+            ["Wellington"],
+            ["Christchurch"],
+            ["Queenstown"],
+            ["London"],
+            ["Bristol"],
+            ["Leeds"],
+            ["Manchester"],
+            ["Liverpool"],
+            ["Glasgow"],
+            ["Edinburgh"],
+            ["Dundee"],
+            ["Belfast"],
+        ]
+        return options
+    end
+
+    def country_select()
+        options = [
+            ["Australia"],
+            ["United Kingdom"], 
+            ["New Zealand"]
         ]
         return options
     end
